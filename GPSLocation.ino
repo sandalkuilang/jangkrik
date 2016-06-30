8b0ca6ede04e8b91ddda16ec32eab6086525eb5e
Hendra Yudha Pratama <yudha_hyp@yahoo.com>
2016-06-30 23:27:07 +0700

    bug fixing device restart

--- GPSLocation.ino
+++ GPSLocation.ino
@@ -15,11 +15,11 @@ by sandalkuilang
 
 SoftwareSerial ss = SoftwareSerial(TX_PIN, RX_PIN); //initialize software serial
 
-String APN = "internet"; //Set APN for Mobile Service for TELKOMSEL 
+String APN = "internet"; //Set APN for Mobile Service for TELKOMSEL
 
 //cloud URL Building
 /*
-https://data.sparkfun.com/streams/NJ65RArGbRUd3A8DNxab
+https://data.sparkfun.com/streams/NJ65RArGbRUd3A8DNxab/5dReDrY9mDhJY0pBPa7g
 */
 const String URL  = "http://data.sparkfun.com/input/";
 const String publicKey  = "NJ65RArGbRUd3A8DNxab"; //Public Key for data stream
@@ -44,27 +44,27 @@ void setup()
 
 	delay(5000); // give 5s GSM device to be ready
 	setupGPS(5); // check gps every 5 second
-	setupGPRS(); 
+	setupGPRS();
 	delay(1000);
 	closeGPRS();
-	
+
 	sendATCommand("AT+CMGD=4", 100); // delete all messages
-	
+
 	Serial.println(F("Proudly Made in Indonesia"));
 }
 
 void loop()
 {
 	if (ss.available())
-	{ 
+	{
 		bool isPublish = false;
 		char c;
 		c = ss.read();
-		if (c == 0x0A || c == 0x0D) 
+		if (c == 0x0A || c == 0x0D)
 		{
-			// Process NMEA string 
+			// Process NMEA string
 			if (content.indexOf("UGNSINF: 1,1") == 1)
-			{ 
+			{
 				if (processGPS(content))
 				{
 					// publish message to server based on speed
@@ -87,7 +87,7 @@ void loop()
 							isPublish = true;
 						}
 					}
-					else if (speed > 2 && speed < 11) 
+					else if (speed > 2 && speed < 11)
 					{
 						tolerate0 = 0;
 						tolerate5 = 0;
@@ -110,7 +110,7 @@ void loop()
 						tolerate1 = 0;
 						tolerate20 = 0;
 
-						tolerate5 += 1; 
+						tolerate5 += 1;
 						if (tolerate5 > 6) // 30 second
 						{
 							isPublish = true;
@@ -139,15 +139,15 @@ void loop()
 						}
 					}
 					else if (speed >= 40)
-					{ 
+					{
 						// 5 second
 						isPublish = true;
 						tolerate0 = 0;
 						tolerate1 = 0;
 						tolerate5 = 0;
 						tolerate20 = 0;
-					} 
-					
+					}
+
 					if (isPublish)
 					{
 						openGPRS();
@@ -157,24 +157,37 @@ void loop()
 
 				}
 			}
+			else if(content.indexOf("+CPIN: READY") == 1)
+			{
+				Serial.begin(9600);
+				ss.begin(9600);
+
+				delay(5000); // give 5s GSM device to be ready
+				setupGPS(5); // check gps every 5 second
+				setupGPRS();
+				delay(1000);
+				closeGPRS();
+
+				sendATCommand("AT+CMGD=4", 100); // delete all messages
+			}
 			else
 			{
 				// process another data, SMS or Call
-			} 
+			}
 			content = String("");
 		}
-		else content.concat(c); 
+		else content.concat(c);
 	}
 }
 
 bool processGPS(String value)
-{ 
+{
 	char frame[100];
 
-	// First get the NMEA string  
-	memset(frame, '\0', 100);    // Initialize the string 
-	 
-	// Parses the string 
+	// First get the NMEA string
+	memset(frame, '\0', 100);    // Initialize the string
+
+	// Parses the string
 	char *strings[30];
 	byte i = 0;
 
@@ -186,7 +199,7 @@ bool processGPS(String value)
 	{
 		strings[i] = strtok(NULL, ",");
 	}
-	 
+
 	if (strstr(strings[1], "1") != NULL)  // Fix status
 	{
 		fieldData[0] = String(strings[1]); // Date
@@ -194,7 +207,7 @@ bool processGPS(String value)
 		fieldData[2] = String(strings[3]); // Longitude
 		fieldData[3] = String(strings[4]); // Altitude
 		fieldData[4] = String(strings[5]); // Speed
-		fieldData[5] = String(strings[6]); // Course 
+		fieldData[5] = String(strings[6]); // Course
 
 #if DEBUG
 		Serial.println();
@@ -206,19 +219,19 @@ bool processGPS(String value)
 		Serial.println("Course : " + fieldData[5]);
 #endif
 		return true;
-	} 
+	}
 	return false;
 }
- 
+
 void makeRequest()
 {
 	//Make HTTP GET request and then close out GPRS connection
 	/* Lots of other options in the HTTP setup, see the datasheet: google -sim800_series_at_command_manual */
-	 
+
 #ifdef DEBUG
 	Serial.print(F("Send URL: "));
 #endif
-	
+
 	sendGetRequest();
 	uint16_t timeOut = 0;
 	while (ss.available() == 0)
@@ -235,13 +248,13 @@ void makeRequest()
 
 #ifdef DEBUG
 	Serial.print(F("Make GET Request: "));
-#endif 
+#endif
 	ss.println("AT+HTTPACTION=0");
 	for (byte d = 0; d <= 50; d++)
 	{
 		delay(100);
 		flushBuffer(); //Flush out the Serial Port
-	} 
+	}
 
 	if (isHttpRead) {
 		ss.println("AT+HTTPREAD");
@@ -269,7 +282,7 @@ bool sendGetRequest() {
 #ifdef DEBUG
 	Serial.print(publicKey);
 #endif
-	
+
 	if (privateKey == NULL)
 	{
 		ss.print("?");
@@ -278,7 +291,7 @@ bool sendGetRequest() {
 #endif
 	}
 	else
-	{ 
+	{
 		ss.print("?private_key=");
 #ifdef DEBUG
 		Serial.print(F("?private_key="));
@@ -288,14 +301,14 @@ bool sendGetRequest() {
 #ifdef DEBUG
 		Serial.print(privateKey);
 #endif
-	} 
+	}
 
-	for (byte i_url = 0; i_url < NUM_FIELDS; i_url++) { 
+	for (byte i_url = 0; i_url < NUM_FIELDS; i_url++) {
 		ss.print("&");
 #ifdef DEBUG
 	Serial.print("&");
-#endif 
-		
+#endif
+
 		ss.print(fieldNames[i_url]);
 #ifdef DEBUG
 		Serial.print(fieldNames[i_url]);
@@ -319,10 +332,10 @@ bool sendGetRequest() {
 #ifdef DEBUG
 	Serial.println();
 #endif
-	return 1; 
+	return 1;
 }
 
-void flushBuffer() { 
+void flushBuffer() {
 	char inChar;
 	for (byte i = 0; i < 10; i++)
 	{
@@ -335,16 +348,16 @@ void flushBuffer() {
 		}
 	}
 }
- 
-void clearBuffer() { 
+
+void clearBuffer() {
 	while (ss.available()) { // do it fast
 		ss.read(); //if there is anything is the serial Buffer, clear it out
 		delay(10);
 	}
 }
- 
-void setupGPS(int interval) 
-{ 
+
+void setupGPS(int interval)
+{
 	char cgnsurcCommand[25];
 	String CGNSURC = "AT+CGNSURC=";
 
@@ -356,10 +369,10 @@ void setupGPS(int interval)
 	CGNSURC.toCharArray(cgnsurcCommand, 25);
 	sendATCommand(cgnsurcCommand, 100); // set interval for automatic return value from GPS device
 }
-  
+
 void setupGPRS()
-{ 	 
-	sendATCommand("ATE0", 100); 
+{
+	sendATCommand("ATE0", 100);
 
 	sendATCommand("AT+CMGF=1", 100);
 
@@ -371,16 +384,16 @@ void setupGPRS()
 	ss.print("AT+SAPBR=3,1,\"APN\",\"");
 	ss.print(APN);
 	ss.print("\"");
-	ss.println(); 
+	ss.println();
 }
 
 void openGPRS()
-{  
+{
 	uint16_t timeOut = 0;
 	sendATCommand("AT+SAPBR=1,1", 100);
 	while (ss.available() == 0)
 	{
-		delay(10); 
+		delay(10);
 		timeOut += 1;
 		if (timeOut >= 350)
 		{
@@ -388,11 +401,11 @@ void openGPRS()
 		}
 	}
 
-	//initialize HTTP service. If it's already on, this will throw an Error. 
+	//initialize HTTP service. If it's already on, this will throw an Error.
 	sendATCommand("AT+HTTPINIT", 50);
 
 	//Mandatory, Bearer profile identifier
-	sendATCommand("AT+HTTPPARA=\"CID\",1", 50); 
+	sendATCommand("AT+HTTPPARA=\"CID\",1", 50);
 }
 
 void closeGPRS()
@@ -404,20 +417,20 @@ void closeGPRS()
 String sendATCommand(char* ATcommand, unsigned int timeout) {
 	unsigned long previous;
 	String result = "";
-	 
+
 	if (ATcommand[0] != '\0')
 	{
-		ss.println(ATcommand);    // Send the AT command 
+		ss.println(ATcommand);    // Send the AT command
 		delay(20);
 	}
-	 
+
 	previous = millis();
 
 	// this loop waits for the answer
 	do {
 		if (ss.available() != 0) {    // if there are data in the UART input buffer, reads it and checks for the asnwer
-			result.concat(ss.read()); 
+			result.concat(ss.read());
 		}
 	} while (((millis() - previous) < timeout));    // Waits for the asnwer with time out
 	return result;
-} 
+}
